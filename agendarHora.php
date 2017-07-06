d821ec34077a0e42f1dde62ff78b498f9832d7e1
Asus <Asus@DESKTOP-JJ8MOS4>
2017-07-06 19:09:45 -0400

    avance

---
+++ agendarHora.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">  
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Inicio</title>
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/estilos.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">Inicio</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <u1 class="nav navbar-nav navbar-right">
+                        <li><a href="cancelarHora.php">Cancelar hora</a></li>
+                    </u1>
+            </div>
+        </nav>
+        
+        <div class="container">
+            <div class="jumbotron">
+                <h1><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Hospital</h1>
+                <p>
+                     Aqui puede agendar una hora y cancelarla
+                </p>
+            </div> 
+        </div>
+        
+      
+               
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js.js"></script>
+    </body>
+</html>
